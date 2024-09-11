db442d9ef49424b567598cebd7647f801ffa47a3
Sakarona Bora <701017313@GoTVDSB.ca>
2024-09-11 09:40:06 -0400

    Add project files.

---
+++ User Input/Program.cs
@@ -0,0 +1,19 @@
+﻿//test with string
+
+using System;
+
+string userName;
+Console.WriteLine("enter you name");
+userName = Console.ReadLine();
+Console.WriteLine("so your name is " +  userName);
+Console.ReadLine();
+
+//test with numbers
+
+int aGe;
+Console.WriteLine("how old are you?");
+aGe = Console.ReadLine();
+Console.WriteLine(int32.TryParse(Console.ReadLine(), out aGe));
+Console.WriteLine("AYO WHAT THE " + aGe + " YOU AINT OLD ENOUGH YET FOOL");
+Console.ReadLine();
+
