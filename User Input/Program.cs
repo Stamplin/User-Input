a7acb26c6dbcab7a433a013b62244205de5aaf2c
Stamplin <701017313@gotvdsb.ca>
2024-09-13 09:38:10 -0400

    Update Program.cs

--- User Input/Program.cs
+++ User Input/Program.cs
@@ -20,16 +20,16 @@ Console.Clear();
 string firstName, lastName, login;
 int grade, ID;
 double average;
-Console.WriteLine("Oi fill out da form for a chance to win 10,000$ dollars.");
-Console.WriteLine("What is your first name? "); 
+Console.WriteLine("Please fill out da form for a chance to win 10,000$ dollars!");
+Console.WriteLine("What is your first name? ");
 firstName = Console.ReadLine();
-Console.WriteLine("What is your last name? "); 
+Console.WriteLine("What is your last name? ");
 lastName = Console.ReadLine();
-Console.WriteLine("What grade are you in? "); 
+Console.WriteLine("What grade are you in? ");
 Int32.TryParse(Console.ReadLine(), out grade);
-Console.WriteLine("What is yout student ID? "); 
+Console.WriteLine("What is yout student ID? ");
 Int32.TryParse(Console.ReadLine(), out ID);
-Console.WriteLine("what is your login?"); 
+Console.WriteLine("what is your login?");
 login = Console.ReadLine();
 Console.WriteLine("What is your average? "); double.TryParse(Console.ReadLine(), out average);
 Console.WriteLine(); //space to seprate the two spaces
@@ -45,8 +45,30 @@ Console.ReadLine();
 Console.Clear();
 
 //part 3:
+string p3Name;
+int p3Age;
+Console.WriteLine("Hey! What is your name?");
+p3Name = Console.ReadLine();
+Console.WriteLine("How old are you?");
+Int32.TryParse(Console.ReadLine(), out p3Age);
+Console.WriteLine("Did you know in 5 years you will be " + (p3Age + 5) + " and 5 years ago you were " + (p3Age - 5));
+Console.WriteLine("press enter to move to part 4");
+Console.ReadLine();
+Console.Clear();
 
+//part 4:
+double calInput, calInput2, calInput3;
+Console.WriteLine("Welcome to the Dumb Calculator \n Please input 3 numbers you would like to added and devided by 2: ");
+Console.WriteLine("input your first number: "); double.TryParse(Console.ReadLine(), out calInput);
+Console.WriteLine("input your second number: "); double.TryParse(Console.ReadLine(), out calInput2);
+Console.WriteLine("input your third number: "); double.TryParse(Console.ReadLine(), out calInput3);
+Console.Clear();
+Console.WriteLine("your 3 numbers added is " + (calInput + calInput2 + calInput3) + ", divided by two is: " + ((calInput + calInput2 + calInput3)/2));
+Console.WriteLine("press enter to move to part 5");
+Console.ReadLine();
+Console.Clear();
 
+//part 5
 
 
 Console.ReadLine();
