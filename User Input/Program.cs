84d14acd6a50450e779de3dbc499639c1739cc57
Stamplin <701017313@gotvdsb.ca>
2024-09-13 08:59:43 -0400

    Update Program.cs

--- User Input/Program.cs
+++ User Input/Program.cs
@@ -17,13 +17,36 @@ Console.ReadLine();
 Console.Clear();
 
 //part 2
-string fname, lname, login;
-int grade, id;
+string firstName, lastName, login;
+int grade, ID;
 double average;
 Console.WriteLine("Oi fill out da form for a chance to win 10,000$ dollars.");
-Console.WriteLine("First name: "); fname = Console.ReadLine();
-Console.WriteLine("Last name: "); lname = Console.ReadLine();
-Console.WriteLine("Grade(9-12): "); fname = Console.ReadLine();
+Console.WriteLine("What is your first name? "); 
+firstName = Console.ReadLine();
+Console.WriteLine("What is your last name? "); 
+lastName = Console.ReadLine();
+Console.WriteLine("What grade are you in? "); 
+Int32.TryParse(Console.ReadLine(), out grade);
+Console.WriteLine("What is yout student ID? "); 
+Int32.TryParse(Console.ReadLine(), out ID);
+Console.WriteLine("what is your login?"); 
+login = Console.ReadLine();
+Console.WriteLine("What is your average? "); double.TryParse(Console.ReadLine(), out average);
+Console.WriteLine(); //space to seprate the two spaces
+Console.WriteLine("Your information:");
+Console.WriteLine("Login:\t\t" + login);
+Console.WriteLine("ID:\t\t" + ID);
+Console.WriteLine("Name:\t\t" + firstName + ", " + lastName);
+Math.Round(average, 1);
+Console.WriteLine("Average:\t" + average + "%");
+Console.WriteLine("Grade:\t\t" + grade);
+Console.WriteLine("press enter to move to part 3");
+Console.ReadLine();
+Console.Clear();
+
+//part 3:
+
+
 
 
 Console.ReadLine();
